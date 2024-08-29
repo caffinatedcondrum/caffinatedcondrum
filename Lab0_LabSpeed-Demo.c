4b99f4db6a389921c54e0922fc5d0dcb7c4c7417
Val // CC <valentinebauer28@gmail.com>
2024-08-29 10:50:16 -0700

    Add files via upload

---
+++ Lab0_LabSpeed-Demo.c
@@ -0,0 +1,12 @@
+ 
+  #include <stdio.h>
+ 
+  int main (int argo,const char:*argV[])
+    {printf("Valentine Bauer!\n")
+      return 0;}
+
+
+    /*Valentine Bauer
+    Lab 0
+    08/28/2024
+    CompSci 112*/
